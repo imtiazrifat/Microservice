ab52a8a07f6fa78f6df9e7a4055d649c36eb5570
Nafiz Rifat <imtiaz_rifat@yahoo.com>
2019-06-11 22:02:18 -0500

    Initial

---
+++ Microservice/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microservice.DBContexts;
+using Microservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+        
+        public ProductRepository (ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int ProductId)
+        {
+            return _dbContext.Products.Find(ProductId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product Product)
+        {
+            _dbContext.Add(Product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product Product)
+        {
+            _dbContext.Entry(Product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
